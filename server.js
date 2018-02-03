b125617dcbbdfa8046867613cd2f542d354d23a3
Venkateshwarlu <vy3191@gmail.com>
2018-02-03 15:39:35 -0500

    Revert "Server set to always respond to index.html file"

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-var path = require('path');
 var express = require("express");
 var app = express();
 //ser port
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
+//set up routes
 
-app.get( "*", function( req, res ) {
-    res.sendfile( path.resolve( __dirname, "index.html" ) );
-} );
+app.get("/", function(request,respond){
+    respond.render("index"); 
+});
 
 app.listen(port, function(){
-	console.log("app is running on port " + port);
+	console.log("app is running");
 });
