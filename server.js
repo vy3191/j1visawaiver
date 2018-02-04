80d729933d2acf54ddad1880e5335552b1d155d9
vy3191 <venkyafhs@gmail.com>
2018-02-04 12:57:52 -0500

    Added Content

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8080;
 app.use(express.static(__dirname));
 
 app.get( "*", function( req, res ) {
-    res.sendfile( path.resolve( __dirname, "index.html" ) );
+    res.sendFile( path.resolve( __dirname, "index.html" ) );
 } );
 
 app.listen(port, function(){
